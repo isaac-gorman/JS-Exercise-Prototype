53003eaa6b44ff3c746adc0083635b9df394de05
Isaac M Gorman <59988845+imgorman@users.noreply.github.com>
2020-05-21 12:27:29 -0500

    DonEzO!

--- index.js
+++ index.js
@@ -39,9 +39,34 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
+function Person(name, age){
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
+};
+// - Create a method using prototype that gives the Person the ability to eat. 
 
-}
+Person.prototype.eat = function(someFood){
+  if (this.stomach.length < 10){
+    this.stomach.push(someFood)
+  } 
+};
+
+// - Create a poop method 
+// - this method when called should empty the array length to 0 items
+
+Person.prototype.poop = function(){
+  // return this.stomach.length = 0 
+  return this.stomach = [];
+};
+
+
+// - Create a method called toString();
+// - toString should return a string with name and age 
+Person.prototype.toString = function(){
+  return `${this.name}, ${this.age}`;
+};
+
 
 /*
   TASK 2
@@ -57,8 +82,17 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
+}
 
+
+Car.prototype.fill = function(gallons){
+  this.tank =  gallons + this.tank;
+  return this.tank;
 }
 
 /*
@@ -68,18 +102,28 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
-
+function Baby(name, age, favoriteToy ) {
+Person.call(this, name, age);
+this.favoriteToy = favoriteToy;
 }
+
+Baby.prototype = Object.create(Person.prototype);
+
+Baby.prototype.play = function(){
+  return `Playing with ${this.favoriteToy}`
+}
+
+
+
 
 /* 
   TASK 4
 
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. Window/ Global Binding: This is the value of "this" in the console wich is the entire JS language.
+  2. Implicit Binding: The most utalized appication of the this keyword. Basically you can identify what "this" is by just looking to the left of the dot. "this" is being called on that object. The ability for the "this" to be called on whatever it is being called on allows for modular code, and results depinding on what the "this" key word is being called on. 
+  3. New Binding: Used with constructor functions "this" keyword refers to specific instance of the object that is created. Takes the value of whaterer argument you pass within the new Object of the constructure function. "this" keyword enalbles constructure functions to be modular and to be able to create new objects depeinding on what is passed as arguments. 
+  4. Explicit Binding: Whenever we specifically apply a method to explictly create subclassed constructors. Any time we use the .call(), .apply(), or .bind() methods. 
 */
 
 
